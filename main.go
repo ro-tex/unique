3a7d53905d39ab2bb5b1ad736e30fd111682e4a3
Ivaylo Novakov <inovakov@gmail.com>
2022-08-18 21:57:51 +0200

    Add support for stdin filtering.

--- main.go
+++ main.go
@@ -5,29 +5,40 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
 )
 
+// readFile opens the given file for reading and returns a reader and a closing
+// function.
+func readFile(fPath string) (r *bufio.Reader, closeFn func() error, err error) {
+	file, err := os.Open(fPath)
+	if err != nil {
+		return nil, nil, err
+	}
+	return bufio.NewReader(file), file.Close, nil
+}
+
 func main() {
-	var fName string
-	flag.StringVar(&fName, "f", "", "name of the file to process")
+	var fPath string
+	flag.StringVar(&fPath, "f", "", "path to the file to process")
 	flag.Parse()
 
-	if fName == "" {
-		flag.Usage()
-		os.Exit(1)
+	var reader *bufio.Reader
+	if fPath != "" {
+		r, closeFn, err := readFile(fPath)
+		if err != nil {
+			log.Fatalf("Failed to read from file '%s'. Error: %s\n", err.Error())
+		}
+		reader = r
+		defer closeFn()
+	} else {
+		reader = bufio.NewReader(os.Stdin)
 	}
-
-	file, err := os.Open(fName)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
 
 	// This map will hold the unique lines
 	m := make(map[string]struct{})
 
-	reader := bufio.NewReader(file)
 	for {
 		line, _, err := reader.ReadLine()
 		if err == io.EOF {
